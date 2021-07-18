16bfda84f6eb7dfb46854114ac3b5363034d2295
Danila <shvedd02@gmail.com>
2021-07-18 21:45:50 +0300

    My first Commit

---
+++ src/SearchArea.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const SearchArea = (props) => {
+  return(
+    <div className="search-area">
+      <form onSubmit={props.searchBook} action="">
+        <input onChange={props.handleSearch} type="text"/>
+        <button id="search" type="submit">Search</button>
+        <select defaultValue="Sort" onChange={props.handleSort}>
+          bled v<option disaalue="Sort">Sort</option>
+          <option value="Newest">Newest</option>
+          <option value="Oldest">Oldest</option>
+        </select>
+      </form>
+    </div>
+
+  )
+}
+
+
+export default SearchArea;
